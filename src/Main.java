fc5c9a9bfb98d132cb660889a464d2be3d429c7c
grifo <grifoncheg1996@gmail.com>
2022-09-23 19:36:22 +0300

    Commit Homework

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+    int dog = (int) 8.0;
+    System.out.println(dog);
+            int cat = 3.6;
+      System.out.println(cat);
+
+            int paper = 763789;
+              System.out.println(cat);
+              int dog = dog + 4;
+              System.out.println(dog);
+
+    int cat = cat + 4;
+    System.out.println(cat);
+
+    var paper = paper + 4;
+    System.out.println(paper);
+
+    int friend = 19.0;
+    System.out.println(friend);
+    int friend = friend * 2;
+    System.out.println(friend);
+    int friend = friend / 7;
+    System.out.println(friend);
+
+
+
+
+
+
+
+
+
 }
+
